247b01cda633015f1da8f28e4fafcd2160b0c697
PR3C14D0 <aarongarpre@gmail.com>
2024-10-22 03:27:11 +0200

    Many improvements on the control of de C2

--- cnc/main.cpp
+++ cnc/main.cpp
@@ -1,24 +1,62 @@
 ﻿#include <iostream>
+#ifdef WIN32
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 #include <Windows.h>
+#endif
 #include "Color.h"
+#include <string>
+#include <csignal>
+
+#define SERVER_IP 127.0.0.1 // TODO: Retrieve IP from database.
 
 void PrintBanner();
 
+enum COMMAND {
+	EXIT = 0xFF,
+	UNKNOWN = 0xF0
+};
+
+COMMAND TranslateCommand(std::string command);
+
+bool g_bQuit = false;
+
+void SignalHandler(int signal);
+
 int main() {
+#ifdef WIN32
+	WSADATA wsa;
+	WSAStartup(MAKEWORD(2, 2), &wsa);
 	SetConsoleOutputCP(CP_UTF8);
+#endif
 	PrintBanner();
 
-	while (true) {
-		std::cout << YELLOW << "\tpreciado >> " << RESET;
-		char input[4];
-		std::cin.getline(input, 4);
-		std::cout << "\033[2J\033[1;1H";
-		std::cout << input << std::endl;
+	std::signal(SIGINT, SignalHandler);
+
+	SOCKET server = socket();
+
+	while (!g_bQuit) {
+		std::cout << YELLOW << "\tpreciado > " << RESET;
+		std::string input;
+		std::getline(std::cin, input);
+
+		COMMAND cmd = TranslateCommand(input);
+
+		switch (cmd) {
+		case EXIT:
+			std::cout << BOLDBLUE << "\tDisconnecting..." << RESET << std::endl;
+			g_bQuit = true;
+			break;
+		case UNKNOWN:
+		default:
+			break;
+		}
 	}
 	return 0;
 }
 
 void PrintBanner() {
+	std::cout << "\033]0;Marlborge Network\007";
 	std::cout << RED << u8R"(
 			⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀
 			⢻⢷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⡇
@@ -51,4 +89,21 @@ void PrintBanner() {
 			⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠲⠤⠞⠋⠀⠙⠶⠤⠖⠋⠁⠀⠀
 
 )" << "\t\t\t\t\t      " BG_RED << BOLDWHITE << "Marlborge Network" << RESET << std::endl;
+}
+
+void SignalHandler(int signal) {
+	if (signal == SIGINT) {
+		std::cout << BOLDBLUE << "\nInterruption received (Ctrl+C). Exiting...\n" << RESET << std::endl;
+		g_bQuit = true;
+	}
+}
+
+COMMAND TranslateCommand(std::string command) {
+	COMMAND cmd = COMMAND::UNKNOWN;
+
+	if (command == "exit") {
+		cmd = COMMAND::EXIT;
+	}
+
+	return cmd;
 }
